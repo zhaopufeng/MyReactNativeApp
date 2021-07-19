2dd960481f9fae298a79ae95b6a5aca8405c71aa
zhaojun <331444850@qq.com>
2021-07-19 17:19:28 +0800

    init

---
+++ index.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import {
+    AppRegistry,
+    StyleSheet,
+    View
+} from 'react-native';
+
+class RNHighScores extends React.Component {
+    render() {
+        // var contents = this.props['scores'].map((score) => (
+        //     <Text key={score.name}>
+        //         {score.name}:{score.value}
+        //         {'\n'}
+        //     </Text>
+        // ));
+        return (
+            <View style={styles.container}>{"esrdfghjgfdsdfghjkhgfdsdfghjkl"}</View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        backgroundColor: '#FFFFFF'
+    },
+    highScoresTitle: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10
+    },
+    scores: {
+        textAlign: 'center',
+        color: '#333333',
+        marginBottom: 5
+    }
+});
+
+// Module name
+AppRegistry.registerComponent('RNHighScores', () => RNHighScores);
